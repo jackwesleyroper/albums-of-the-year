e95f7249c497e20f8be017c21e3131d72728bc71
Jack Roper <39914569+jackwesleyroper@users.noreply.github.com>
2025-09-25 09:21:27 +0100

    update ids

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,23 @@
-
 function loadAlbums() {
-    const year = document.getElementById("yearSelect").value;
+    const year = document.getElementById("year-select").value;
     fetch(`data/${year}.json`)
-        .then(response => response.json())
-        .then(albums => {
-            const container = document.getElementById("albumsContainer");
-            container.innerHTML = "";
-            albums.forEach(album => {
-                const div = document.createElement("div");
-                div.className = "album";
-                const img = document.createElement("img");
-                img.src = `https://coverartarchive.org/release/${album.mbid}/front`;
-                img.alt = album.title;
-                const title = document.createElement("p");
-                title.textContent = album.title;
-                div.appendChild(img);
-                div.appendChild(title);
-                container.appendChild(div);
-            });
+      .then(response => response.json())
+      .then(albums => {
+        const container = document.getElementById("albums-container");
+        container.innerHTML = "";
+        albums.forEach(album => {
+          const div = document.createElement("div");
+          div.className = "album";
+          const img = document.createElement("img");
+          img.src = `https://coverartarchive.org/release/${album.mbid}/front`;
+          img.alt = album.title;
+          const title = document.createElement("p");
+          title.textContent = album.title;
+          div.appendChild(img);
+          div.appendChild(title);
+          container.appendChild(div);
         });
-}
-window.onload = loadAlbums;
+      });
+  }
+  window.onload = loadAlbums;
+  
